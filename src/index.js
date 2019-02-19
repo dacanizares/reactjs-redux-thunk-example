cb1908bf57acbf03f4ed9e9a1aecdd367c3903dd
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-02-18 21:07:48 -0500

    Fixing routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from '../components/App';
+import App from './components/App';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers'
 import { Provider } from 'react-redux'
